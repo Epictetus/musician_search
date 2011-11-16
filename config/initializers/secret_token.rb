f938bd7b08f81dae2263dc38c4404d92826c9093
fumihiro <fumi1985711@gmail.com>
2011-11-16 20:13:23 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtistSearch::Application.config.secret_token = '160d7f50843cfe84ba1545f8659f7b0232bc56538550963db7e4b649964d052a9eb5e7a5623ab4700fefcd4f49acae8404f52071472958689144a219d01345af'
