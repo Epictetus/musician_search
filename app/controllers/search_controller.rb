7d84b4d5f12b38a5ce22a5036bdc979c26301ab7
fumihiro <fumi1985711@gmail.com>
2012-02-26 18:06:32 +0900

    wikipediaの情報を取得＆表示する機能を実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,27 +15,26 @@ class SearchController < ApplicationController
     search_word = params[:search_keys][:name] 
     page = Wikipedia.find(search_word)
     @title = page.title
-    # categories = page.categories[0]
     has_many_choices = false
-    page.categories.each {|val| has_many_choices = true unless val.index("曖昧") == nil }
+    page.categories.each {|val| has_many_choices = true unless val.index("曖昧") == nil } unless page.categories.blank?
 
     content = WikiCloth::Parser.new(data: page.content)
     @wiki_content = Sanitize.clean(content.to_html).gsub(/\n/, "").gsub(/\" \^/, "").gsub(/\"/, "")
     if @wiki_content.blank?
-      @wiki_content = "Sorry, not found content."
-      @wiki_content += " Please check the following link." unless page.content.blank?
+      @wiki_content = "該当する情報は見つかりませんでした…"
+      @wiki_content += " 下記のリンクより詳細をご確認ください。" unless page.content.blank?
     else
       if has_many_choices
-        @wiki_content = "Returned many result. Please check the following link."
+        @wiki_content = "複数項目が該当しました。下記のリンクより詳細をご確認ください。"
       else
-        @wiki_content = "introduction:<br />" + @wiki_content[0..150].gsub(/[。]/, "。<br />") + "..."
+        @wiki_content = "<h3>#{@title}について:</h3>" + @wiki_content[0..150].gsub(/[。]/, "。<br />") + "..."
       end
     end
 
     if page.content.blank?
-      @wiki_link = "searched word:" + search_word
+      @wiki_link = "検索ワード:" + search_word
     else
-      @wiki_link = "detail:<a href=http://ja.wikipedia.org/wiki/#{search_word}>#{@title}</a>"
+      @wiki_link = "詳細はこちら:<a href=http://ja.wikipedia.org/wiki/#{search_word}>#{@title}</a>"
     end
 
     render template: "search/index"
